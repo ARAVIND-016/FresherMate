416fcbb57ff1682bf23c5a6052e5b13c583bff8a
ARAVIND-016 <aravindoffcl7@gmail.com>
2024-09-02 10:28:15 +0530

    Add files via upload

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>FresherMate</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Friend</span></p>
+                    <p>How can I help you Today</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box"> 
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        FresherMate may display info.including about people,so double-check its respones.
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Main
